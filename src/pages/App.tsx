0389c11638771012ac06e257b4c8e6f9b816552d
Mootzy <tyler77156@gmail.com>
2021-11-19 21:41:39 -0700

    first commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,191 @@
+import React, { Suspense, useEffect, useState } from 'react'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import styled from 'styled-components'
+import { Credentials, StringTranslations } from '@crowdin/crowdin-api-client'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Popups from '../components/Popups'
+import Web3ReactManager from '../components/Web3ReactManager'
+import { RedirectDuplicateTokenIds, RedirectOldAddLiquidityPathStructure } from './AddLiquidity/redirects'
+import { RedirectOldRemoveLiquidityPathStructure } from './RemoveLiquidity/redirects'
+import AddLiquidity from './AddLiquidity'
+import Pool from './Pool'
+import PoolFinder from './PoolFinder'
+import RemoveLiquidity from './RemoveLiquidity'
+import Swap from './Swap'
+import { RedirectPathToSwapOnly } from './Swap/redirects'
+import { EN, allLanguages } from '../constants/localisation/languageCodes'
+import { LanguageContext } from '../hooks/LanguageContext'
+import { TranslationsContext } from '../hooks/TranslationsContext'
+
+
+import Menu from '../components/Menu';
+
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: flex-start;
+  overflow-x: hidden;
+`
+
+const BodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 32px 16px;
+  align-items: center;
+  flex: 1;
+  overflow-y: auto;
+  overflow-x: hidden;
+  z-index: 1;
+  justify-content: center;
+  background-image: url('/images/left-pancake.svg');
+  background-repeat: no-repeat;
+  background-position: bottom 0px center;
+  background-size: 90%;
+
+  ${({ theme }) => theme.mediaQueries.xs} {
+    background-size: auto;
+  }
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    background-image: url('/images/arch-${({ theme }) => (theme.isDark ? 'dark' : 'light')}.svg'),
+    url('/images/left-pancake.svg'), url('/images/right-pancake.svg');
+    background-repeat: no-repeat;
+    background-position: center 420px, 10% 230px, 90% 230px;
+    background-size: contain, 500px, 500px;
+    min-height: 90vh;
+  }
+`
+
+const Marginer = styled.div`
+  margin-top: 5rem;
+`
+
+export default function App() {
+  const [usCitizenDialogOpen, setUsCitzenDialogOpen] = React.useState(true);
+  const [selectedLanguage, setSelectedLanguage] = useState<any>(undefined)
+  const [translatedLanguage, setTranslatedLanguage] = useState<any>(undefined)
+  const [translations, setTranslations] = useState<Array<any>>([])
+  const apiKey = `${process.env.REACT_APP_CROWDIN_APIKEY}`
+  const projectId = parseInt(`${process.env.REACT_APP_CROWDIN_PROJECTID}`)
+  const fileId = 6
+
+  const credentials: Credentials = {
+    token: apiKey,
+  }
+
+  const handleClose = () => {
+    setUsCitzenDialogOpen(false);
+  };
+
+  const stringTranslationsApi = new StringTranslations(credentials)
+
+  const getStoredLang = (storedLangCode: string) => {
+    return allLanguages.filter((language) => {
+      return language.code === storedLangCode
+    })[0]
+  }
+
+  useEffect(() => {
+    const storedLangCode = localStorage.getItem('pancakeSwapLanguage')
+    if (storedLangCode) {
+      const storedLang = getStoredLang(storedLangCode)
+      setSelectedLanguage(storedLang)
+    } else {
+      setSelectedLanguage(EN)
+    }
+  }, [])
+
+  const fetchTranslationsForSelectedLanguage = async () => {
+    stringTranslationsApi
+      .listLanguageTranslations(projectId, selectedLanguage.code, undefined, fileId, 200)
+      .then((translationApiResponse) => {
+        if (translationApiResponse.data.length < 1) {
+          setTranslations(['error'])
+        } else {
+          setTranslations(translationApiResponse.data)
+        }
+      })
+      .then(() => setTranslatedLanguage(selectedLanguage))
+      .catch((error) => {
+        setTranslations(['error'])
+        console.error(error)
+      })
+  }
+
+  useEffect(() => {
+    if (selectedLanguage) {
+      fetchTranslationsForSelectedLanguage()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedLanguage])
+
+  return (
+    <Suspense fallback={null}>
+      <HashRouter>
+        <AppWrapper>
+          <LanguageContext.Provider
+            value={{ selectedLanguage, setSelectedLanguage, translatedLanguage, setTranslatedLanguage }}
+          >
+            <TranslationsContext.Provider value={{ translations, setTranslations }}>
+              <Menu>
+                <BodyWrapper>
+                  <Popups />
+                  <Web3ReactManager>
+                    <Switch>
+                      <Route exact strict path="/swap" component={Swap} />
+                      <Route exact strict path="/find" component={PoolFinder} />
+                      <Route exact strict path="/pool" component={Pool} />
+                      <Route exact path="/add" component={AddLiquidity} />
+                      <Route exact strict path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
+
+                      {/* Redirection: These old routes are still used in the code base */}
+                      <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
+                      <Route exact path="/add/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
+                      <Route exact strict path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
+
+                      <Route component={RedirectPathToSwapOnly} />
+                    </Switch>
+                  </Web3ReactManager>
+                  <Marginer />
+                </BodyWrapper>
+              </Menu>
+              <Dialog
+                open={usCitizenDialogOpen}
+                onClose={(event, reason) => {
+                  if (reason !== "backdropClick") {
+                    handleClose();
+                  }
+                }}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+              >
+                <DialogTitle id="alert-dialog-title">
+                  Liability Waiver / Disclaimer: 
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    <p>No content on the site constitutes - or should be understood as constituting - a recommendation to enter in any securities, cryptocurrency, or cryptoasset transactions or to engage in any of the investment strategies presented in our online content. The services on universal-floki are not intended for use in the countries listed below. We do NOT hold any liability over any financial gain or loss due to usage of univerflokiswap, if you are a user from the below regions:</p>
+                    <br />
+                    <p>United States of America, China Mainland, Albania, Bosnia and Herzegovina, Belarus, Congo (DRC), Côte d&apos;Ivoire, Crimea, Cuba, Iraq, Iran, North Korea, Liberia, Macedonia, Myanmar, Serbia, Sudan, Syria, Zimbabwe.
+                      <br />
+                        <a href ="https://universalfloki.com/disclaimer">!!!READ OUR FULL DISCLAIMER HERE!!!</a></p>
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button variant="contained" color="primary" onClick={handleClose}>I accept</Button>
+                </DialogActions>
+              </Dialog>
+            </TranslationsContext.Provider>
+          </LanguageContext.Provider>
+        </AppWrapper>
+      </HashRouter>
+    </Suspense>
+  )
+}
